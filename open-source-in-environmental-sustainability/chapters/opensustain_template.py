0bf837ced84800da5236cf7ff7c1bf0710fd88e3
joshhopkins <josh@hoppie.co>
2022-11-28 22:05:11 +0800

    Add color palettes

--- open-source-in-environmental-sustainability/chapters/opensustain_template.py
+++ open-source-in-environmental-sustainability/chapters/opensustain_template.py
@@ -7,7 +7,9 @@ logo_img = Image.open('logo.png') # image path
 # Palette https://coolors.co/palette/0e7c7b-17bebb-ffc857-e9724c-c5283d
 color_continuous_scale = px.colors.sequential.Aggrnyl[::-1]
 marker_color = "#0E7C7B"
-color_discrete_sequence = ["#0E7C7B", "#17BEBB", "#FFC857", "#E9724C", "#C5283D"]
+color_discrete_sequence = ["#3e59fb","#007dff","#0097ff","#00acff","#00bef9","#00cdde","#00dac1","#26e5a9"]
+color_single_hue_scale = ["#159485","#3ca394","#58b1a3","#70c0b3","#87cfc3","#9edfd3","#b5eee3"]
+color_divergent_scale = ["#3e59fb","#9089f9","#c5bcf6","#f1f1f1","#bfeed8","#86eac1","#26e5a9"]
 
 # Register your theme as a named template
 pio.templates["OpenSustain"] = go.layout.Template(
