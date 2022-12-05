1d105d3538d59b4b4949b3f421834129ad552dee
Tobias Augspurger <ly0@protonmail.com>
2022-12-05 20:25:09 +0100

    fine tune colors

--- open-source-in-environmental-sustainability/chapters/opensustain_template.py
+++ open-source-in-environmental-sustainability/chapters/opensustain_template.py
@@ -2,26 +2,26 @@ import plotly.io as pio
 import plotly.graph_objects as go
 import plotly.express as px
 from PIL import Image
-logo_img = Image.open('logo.png') # image path
+
+logo_img = Image.open("logo.png")  # image path
 # default plotting options
 # Palette https://coolors.co/palette/0e7c7b-17bebb-ffc857-e9724c-c5283d
 marker_color = "#0E7C7B"
 color_continuous_scale = px.colors.sequential.Aggrnyl[::-1]
 color_discrete_sequence = ["#0E7C7B", "#17BEBB", "#FFC857", "#E9724C", "#C5283D"]
+boarder_color = "#000000"
 
 # OpenSustain.Tech colours
-#marker_color = "#159485"
-#boarder_color = "#E5ECF6"
-#color_continuous_scale = px.colors.make_colorscale(["#26E5A9","#3E59FB"])
-#color_divergent_scale = px.colors.make_colorscale(["#26E5A9", "#E5ECF6","#3E59FB"])
-#color_discrete_sequence = ["#3e59fb","#007dff","#0097ff","#00acff","#00bef9","#00cdde","#00dac1","#26e5a9"]
+# marker_color = "#159485"
+# boarder_color = "#000000"
+# color_continuous_scale = px.colors.make_colorscale(["#26E5A9","#3E59FB"])
+# color_divergent_scale = px.colors.make_colorscale(["#26E5A9", "#E5ECF6","#3E59FB"])
+# color_discrete_sequence = ["#3e59fb","#007dff","#0097ff","#00acff","#00bef9","#00cdde","#00dac1","#26e5a9"]
 
 # Register your theme as a named template
 pio.templates["OpenSustain"] = go.layout.Template(
     layout=dict(
-        margin=dict(
-        b=0 #bottom margin
-    ),
+        margin=dict(b=0),  # bottom margin
         font=dict(
             family="Open Sans",
             color="#040404",
