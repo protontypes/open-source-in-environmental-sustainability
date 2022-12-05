b2d3ccddd23c1e4d7a9fed2d40d65d1a9d79352d
joshhopkins <josh@hoppie.co>
2022-12-05 21:37:44 +0800

    Consistent colours

--- open-source-in-environmental-sustainability/chapters/opensustain_template.py
+++ open-source-in-environmental-sustainability/chapters/opensustain_template.py
@@ -5,14 +5,16 @@ from PIL import Image
 logo_img = Image.open('logo.png') # image path
 # default plotting options
 # Palette https://coolors.co/palette/0e7c7b-17bebb-ffc857-e9724c-c5283d
-color_continuous_scale = px.colors.sequential.Aggrnyl[::-1]
-marker_color = "#0E7C7B"
-color_discrete_sequence = ["#0E7C7B", "#17BEBB", "#FFC857", "#E9724C", "#C5283D"]
+# marker_color = "#0E7C7B"
+# color_continuous_scale = px.colors.sequential.Aggrnyl[::-1]
+# color_discrete_sequence = ["#0E7C7B", "#17BEBB", "#FFC857", "#E9724C", "#C5283D"]
 
-# Josh
-#color_discrete_sequence = ["#3e59fb","#007dff","#0097ff","#00acff","#00bef9","#00cdde","#00dac1","#26e5a9"]
-#color_single_hue_scale = ["#159485","#3ca394","#58b1a3","#70c0b3","#87cfc3","#9edfd3","#b5eee3"]
-#color_divergent_scale = ["#3e59fb","#9089f9","#c5bcf6","#f1f1f1","#bfeed8","#86eac1","#26e5a9"]
+# OpenSustain.Tech colours
+marker_color = "#159485"
+boarder_color = "#E5ECF6"
+color_continuous_scale = px.colors.make_colorscale(["#26E5A9","#3E59FB"])
+color_divergent_scale = px.colors.make_colorscale(["#26E5A9", "#E5ECF6","#3E59FB"])
+color_discrete_sequence = ["#3e59fb","#007dff","#0097ff","#00acff","#00bef9","#00cdde","#00dac1","#26e5a9"]
 
 # Register your theme as a named template
 pio.templates["OpenSustain"] = go.layout.Template(
