3e1440febba61649728c97b4c592c7cdfba1b545
Tobias Augspurger <ly0@protonmail.com>
2023-01-21 10:31:00 +0100

    blue colors style

--- open-source-in-environmental-sustainability/chapters/opensustainTemplate.py
+++ open-source-in-environmental-sustainability/chapters/opensustainTemplate.py
@@ -6,27 +6,18 @@ from PIL import Image
 logo_img = Image.open("logo.png")  # image path
 # default plotting options
 # Palette https://coolors.co/palette/0e7c7b-17bebb-ffc857-e9724c-c5283d
-#marker_color = "#0E7C7B"
-#color_continuous_scale = [[0, "#3b52ff"], [1, "#169485"]]
-#color_discrete_sequence = ["#3361e4","#2b6fc9","#227eae","#1a8d93","#169485"]
-#boarder_color = "#040404"
+marker_color = "#0E7C7B"
+color_continuous_scale = [[0, "#3b52ff"], [1, "#169485"]]
+color_discrete_sequence = ["#3361e4","#2b6fc9","#227eae","#1a8d93","#169485"]
+boarder_color = "#040404"
 color_divergent_scale = [[0, "#3b52ff"], [0.5, "#f1f1f1"], [1, "#169485"]]
 
 # OpenSustain.Tech colours
-#marker_color = "#159485"
-#boarder_color = "#000000"
-#color_continuous_scale = px.colors.make_colorscale(["#26E5A9","#3E59FB"])
-#color_divergent_scale = px.colors.make_colorscale(["#26E5A9", "#E5ECF6","#3E59FB"])
-#color_discrete_sequence = ["#3e59fb","#007dff","#0097ff","#00acff","#00bef9","#00cdde","#00dac1","#26e5a9"]
-
-# Warm colors
-# Palette https://coolors.co/palette/0e7c7b-17bebb-ffc857-e9724c-c5283d
-marker_color = "#0E7C7B"
-color_continuous_scale = px.colors.sequential.Aggrnyl[::-1]
-color_discrete_sequence = ["#0E7C7B", "#17BEBB", "#FFC857", "#E9724C", "#C5283D"]
-boarder_color = "#000000"
-color_divergent_scale = [[0, "#3b52ff"], [0.5, "#f1f1f1"], [1, "#169485"]]
-
+# marker_color = "#159485"
+# boarder_color = "#000000"
+# color_continuous_scale = px.colors.make_colorscale(["#26E5A9","#3E59FB"])
+# color_divergent_scale = px.colors.make_colorscale(["#26E5A9", "#E5ECF6","#3E59FB"])
+# color_discrete_sequence = ["#3e59fb","#007dff","#0097ff","#00acff","#00bef9","#00cdde","#00dac1","#26e5a9"]
 
 # Register your theme as a named template
 pio.templates["OpenSustain"] = go.layout.Template(
